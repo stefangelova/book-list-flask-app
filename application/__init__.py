6928a0b4542262c6e255cdab2ec26daebff7d643
Ubuntu <stef@new.tupzve51y15ubjclt42lj1l1sb.gx.internal.cloudapp.net>
2020-03-01 13:11:55 +0000

    navbar

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,13 @@
-# import Flask class from the flask module
 from flask import Flask
- # create a new instance of Flask and store it in app 
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
 app = Flask(__name__)
- # import the ./application/routes.py file
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://stef@project1db:Thisisstup1d@project1db.mysql.database.azure.com"
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SECRET_KEY'] = 'YOU_SECRET_KEY'
+#db = SQLAlchemy(app)
+
 from application import routes
 
