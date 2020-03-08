2f9c370cf9a18c656c135ef5c1fb02de5039a99f
Ubuntu <stef@new.tupzve51y15ubjclt42lj1l1sb.gx.internal.cloudapp.net>
2020-03-08 17:18:54 +0000

    secret key change

--- application/__init__.py
+++ application/__init__.py
@@ -8,9 +8,9 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://stef@project1db:Thisisstup1d@project1db.mysql.database.azure.com/proj1db"
+app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+app.config['SECRET_KEY'] = str(getenv('YOUR_SECRET_KEY'))
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
